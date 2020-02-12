f93034134944a6ba770222a478f8a41caf131478
Artiom <sinigur2006@gmail.com>
2020-02-11 20:02:04 -0500

    Challenge: Add some text to notes.txt

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,1 +1,6 @@
-false.writeFileSync('notes.txt', 'This file was created by NodeJs');
+// Load File System
+const fs = require('fs');
+// fs.writeFileSync('notes.txt', 'This file was created and changed by NodeJs');
+
+// Add some text to notes.txt
+fs.appendFileSync('notes.txt', 'This text was added with NodeJs');
