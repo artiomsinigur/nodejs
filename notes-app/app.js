582baa389830fb77ef0efc3b3eee5ae9cf236b67
Artiom <sinigur2006@gmail.com>
2020-02-12 12:16:11 -0500

    Challange: Setup command option to remove and function

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,62 @@
 const {log, error, success, danger} = require('./chalk');
 const yargs = require('./yargs');
-const getNotes = require('./notes');
+const {getNotes, addNote, removeNote} = require('./notes');
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Store new resource',
+    builder: {
+        title: {
+            describe: 'Resource title',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'Body content',
+            demandOption: false,
+            type: 'string', 
+        }
+    },
+    handler: function (argv) {
+        addNote(argv.title, argv.body);
+    }
+});
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Destroy a resource',
+    builder: {
+        title: {
+            describe: 'Resource title',
+            demandOption: true,
+            type: 'string'
+        },
+    },
+    handler: function (argv) {
+        console.log(danger('Removed' + argv.title));
+        removeNote(argv.title);
+    }
+});
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all resources',
+    handler: function () {
+        console.log(log('Listing all resources!'));
+    }
+});
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a resource',
+    handler: function () {
+        console.log(log('Reading a resource!'));
+    }
+});
+
+// Parsing the arguments
+yargs.parse();
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,50 @@
+const fs = require('fs');
+
 const getNotes = function() {
     return 'My notes...';
 }
 
-module.exports = getNotes;
+// Store new resource
+const addNote = function(title, body) {
+    // If problem with loadData return an empty array and stop
+    const notes = loadNotes();
+
+    // Check if note title exist already
+    const duplicateNotes = notes.filter(note => {
+        return note.title === title;
+    });
+
+    // Prevents duplicated notes to be added
+    if (duplicateNotes.length > 0) {
+        console.log('Duplicate of notes');
+    } else {
+        // Otherwise store data
+        notes.push({title, body});
+        storeNotes(notes);
+        console.log('Notes was stored successfully!');
+    }
+}
+
+// Load data from the file
+const loadNotes = function() {
+    try {
+        // Read json data
+        const dataJSON = fs.readFileSync('notes.json').toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        // if file not exist, return an empty array and create an empty file 
+        return [];
+    }
+}
+
+// Write data into file
+const storeNotes = function(notes) {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+const removeNote = function(note) {
+    console.log('File was removed' + note);
+}
+
+module.exports = {getNotes, addNote, removeNote};
