a01c30936067f7335bcd7438367e13f54a797d9b
Artiom <sinigur2006@gmail.com>
2020-02-13 23:46:42 -0500

    Challenge: Handler different type of  errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,33 @@
+const {error, success} = require('../notes-app/chalk');
 const request = require('request');
 
-const urlForecast = 'https://api.darksky.net/forecast/75d2b09a4815455f3e41ecddc78b67cd/37.8267,-122.4233?units=si&lang=fr&timezone=America/Toronto';
+// const urlForecast = 'https://api.darksky.net/forecast/75d2b09a4815455f3e41ecddc78b67cd/37.8267,-122.4233?units=si&lang=fr&timezone=America/Toronto';
 
-request({url: urlForecast, json: true}, (err, res) => {
-    const weather = res.body.currently;
-    const daily = res.body.daily.data;
-    const textNode = `It is currently ${weather.temperature} degrees out. There is a ${weather.precipProbability}%. ${daily[0].summary}`;
-    // console.log(textNode);
-});
+// request({url: urlForecast, json: true}, (err, res) => {
+//     if (err) {
+//         console.log(error('Unable to connect to weather service!'));
+//     } else if (res.body.error) {
+//         console.log(error(res.body.error));
+//     } else {
+//         const weather = res.body.currently;
+//         const daily = res.body.daily.data;
+//         const textNode = `It is currently ${weather.temperature} degrees out. There is a ${weather.precipProbability}%. ${daily[0].summary}`;
+//         console.log(success(textNode));
+//     }
+// });
 
 const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Montr%C3%A9al.json?access_token=pk.eyJ1IjoiYXJ0ZGV2IiwiYSI6ImNrNmxsbWt1ZDBkYTQzbW4wOWhhNHFma20ifQ.sXo-BYsA3ZJCIeo-iNsLYw&limit=1';
 
 request({url: geocodeUrl, json: true}, (err, res) => {
-    const latitude = res.body.features[0].center[0];
-    const longitude = res.body.features[0].center[1];
-    console.log(latitude, longitude);
+    if (err) {
+        console.log(error('Unable to connect to weather service!'));
+    } else if (res.body.message) {
+        console.log(error(res.body.message));
+    } else if (res.body.features.length === 0) {
+        console.log(error('Unable to find location. Try another search!'));
+    } else {
+        const latitude = res.body.features[0].center[0];
+        const longitude = res.body.features[0].center[1];
+        console.log(latitude, longitude);
+    }
 });
