f55f2a940365932feb3b8fe9940779bed96cdc3c
Artiom <sinigur2006@gmail.com>
2020-02-19 21:41:07 -0500

    Challenge write address into input terminal

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,33 +1,25 @@
 const {error, success} = require('../notes-app/chalk');
-const request = require('request');
+const getGeocoding = require('./utils/geocoding'); 
+const getForecast = require('./utils/forecast'); 
 
-// const urlForecast = 'https://api.darksky.net/forecast/75d2b09a4815455f3e41ecddc78b67cd/37.8267,-122.4233?units=si&lang=fr&timezone=America/Toronto';
+let address = process.argv[2];
 
-// request({url: urlForecast, json: true}, (err, res) => {
-//     if (err) {
-//         console.log(error('Unable to connect to weather service!'));
-//     } else if (res.body.error) {
-//         console.log(error(res.body.error));
-//     } else {
-//         const weather = res.body.currently;
-//         const daily = res.body.daily.data;
-//         const textNode = `It is currently ${weather.temperature} degrees out. There is a ${weather.precipProbability}%. ${daily[0].summary}`;
-//         console.log(success(textNode));
-//     }
-// });
-
-const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Montr%C3%A9al.json?access_token=pk.eyJ1IjoiYXJ0ZGV2IiwiYSI6ImNrNmxsbWt1ZDBkYTQzbW4wOWhhNHFma20ifQ.sXo-BYsA3ZJCIeo-iNsLYw&limit=1';
-
-request({url: geocodeUrl, json: true}, (err, res) => {
+getGeocoding(address, (err, data) => {
     if (err) {
-        console.log(error('Unable to connect to weather service!'));
-    } else if (res.body.message) {
-        console.log(error(res.body.message));
-    } else if (res.body.features.length === 0) {
-        console.log(error('Unable to find location. Try another search!'));
-    } else {
-        const latitude = res.body.features[0].center[0];
-        const longitude = res.body.features[0].center[1];
-        console.log(latitude, longitude);
+        return console.log(error(err));
     }
+    
+    getForecast(data, (err, forecastData) => {
+        if (err) {
+            return console.log(error(err));
+        }
+        console.log(success(data.placeName));
+        console.log(success(forecastData));
+    });
 });
+
+// const geocode = getGeocoding('Chisinau', getGeocode);
+// function getGeocode(err, data) {
+    //     return data;
+// }
+// console.log(geocode);
