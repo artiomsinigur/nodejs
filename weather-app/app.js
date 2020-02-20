58d8d18331141fedc2d43a1d1185287cb4fbd72a
Artiom <sinigur2006@gmail.com>
2020-02-19 22:13:24 -0500

    Refactory weather app with destructuring

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,22 +4,16 @@ const getForecast = require('./utils/forecast');
 
 let address = process.argv[2];
 
-getGeocoding(address, (err, data) => {
+getGeocoding(address, (err, {longitude, latitude, placeName}) => {
     if (err) {
         return console.log(error(err));
     }
     
-    getForecast(data, (err, forecastData) => {
+    getForecast(longitude, latitude, (err, forecastData) => {
         if (err) {
             return console.log(error(err));
         }
-        console.log(success(data.placeName));
+        console.log(success(placeName));
         console.log(success(forecastData));
     });
 });
-
-// const geocode = getGeocoding('Chisinau', getGeocode);
-// function getGeocode(err, data) {
-    //     return data;
-// }
-// console.log(geocode);
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,16 +1,16 @@
 const request = require('request');
 
-function getForecast(geocode, callback) {
-    const urlForecast = 'https://api.darksky.net/forecast/75d2b09a4815455f3e41ecddc78b67cd/' + geocode.longitude + ',' + geocode.latitude + '?units=si&lang=fr';
+function getForecast(longitude, latitude, callback) {
+    const urlForecast = 'https://api.darksky.net/forecast/75d2b09a4815455f3e41ecddc78b67cd/' + longitude + ',' + latitude + '?units=si&lang=fr';
     
-    request({url: urlForecast, json: true}, (err, res) => {
+    request({url: urlForecast, json: true}, (err, {body}) => {
         if (err) {
             callback('Unable to connect to weather service!', undefined);
-        } else if (res.body.error) {
-            callback(res.body.error, undefined);
+        } else if (body.error) {
+            callback(body.error, undefined);
         } else {
-            const weather = res.body.currently;
-            const daily = res.body.daily.data;
+            const weather = body.currently;
+            const daily = body.daily.data;
             const textNode = `It is currently ${weather.temperature} degrees out. There is a ${weather.precipProbability}%. ${daily[0].summary}`;
             callback(undefined, textNode);
         }
