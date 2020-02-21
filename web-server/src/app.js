abffe8932fcb9a5ba2bd4f93325a58861d74c14c
Artiom <sinigur2006@gmail.com>
2020-02-21 17:29:00 -0500

    Get path with path.join()

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,30 @@
+const path = require('path'); // Usually we include path before express
 const express = require('express');
+// const getGeocoding = require('../../weather-app/utils/geocoding');
+// const getForecast = require('../../weather-app/utils/forecast');
 
 const app = express();
 
+// Get the path to public folder to send index.html file
+const publicDirectoryPath = path.join(__dirname, '../public');
+// app.use() - customize the server
+app.use(express.static(publicDirectoryPath));
+
 // req - incoming request to the server
 // res - response have a bunch of methods that allow us to customize what we want to send to requester
-// app.com
-app.get('', (req, res) => {
-    res.send('Hello world!');
-});
 // app.com/about
 app.get('/about', (req, res) => {
-    res.send('You are on page about');
+    res.send({
+        name: 'Andrew',
+        age: 31,
+    });
 });
 // app.com/weather
 app.get('/weather', (req, res) => {
-    res.send('You are on page weather');
+    res.send({
+        forecast: 'Forecast',
+        location: 'Montreal',
+    });
 });
 
 // Start server
