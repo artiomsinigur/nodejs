257a5e9bec3527973308fd4f88f3da22c920d91b
Artiom <sinigur2006@gmail.com>
2020-02-22 14:04:55 -0500

    Fix problem for destructuring undefined with = {}

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -6,14 +6,17 @@ const product = {
 
 // : Give another name for property
 // = Set a default value
-const {label: productLabel, price, stock, rating = 5} = product;
-console.log(productLabel);
-console.log(price);
-console.log(rating);
+// const {label: productLabel, price, stock, rating = 5} = product;
+// console.log(productLabel);
+// console.log(price);
+// console.log(rating);
 
 // Take only the properties that you need
-const transaction = (type, {label, price}) => {
+const transaction = (type, {label, price = 0} = {}) => {
     console.log(label, price);
 };
 
-transaction('Order', product);
+// transaction('Order', product);
+
+// If we omit product object destructuring can't destruct undefined. We need to give an empty object = {}
+transaction('Order');
