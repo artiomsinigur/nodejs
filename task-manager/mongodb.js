e738e6bc0b593d50cf4856b4d1d816ccdd0d10cf
Artiom <sinigur2006@gmail.com>
2020-02-25 16:37:50 -0500

    Challenge Read one orr more documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -8,6 +8,13 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
+// Generate a new id
+const ObjectID = mongodb.ObjectID;
+// const id = new ObjectID();
+// console.log(id);
+// console.log(id.getTimestamp());
+
+
 // Connecting to the database
 MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
     if (err) {
@@ -17,52 +24,61 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
     // Create a DB directly hire. No need to create it in mongodb GUI 
     const db = client.db(databaseName);
 
-    // Insert an single document
-    /* db.collection('users').insertOne({
-        name: 'Artiom',
-        age: 33,
-        hobbit: ['Soccer', 'Tennis']
-    }, (err, res) => {
-        if (err) {
-            return console.log('Unable to insert user!');
-        }
+    // CREATE
+    // ==============================
+        // Insert an single document
+        /* db.collection('users').insertOne({
+            name: 'John',
+            age: 33,
+        }, (err, res) => {
+            if (err) {
+                return console.log('Unable to insert user!');
+            }
 
-        // ops is array of documents
-        console.log(res.ops, res.insertedCount);
-    });
+            // ops is array of documents
+            console.log(res.ops, res.insertedCount);
+        }); */
 
-    // Insert an many documents
-    db.collection('users').insertMany([
-        {
-            name: 'Andrew',
-            age: 18
-        }, {
-            name: 'Anton',
-            age: 39
-        }
-    ], (err, res) => {
-        if (err) {
-            return console.log('Unable to insert user!');
-        }
-        console.log(res.ops, res.insertedCount);
-    }); */
+        // Create new tasks collection
+        // Insert an many documents
+        /* db.collection('tasks').insertMany([
+            {
+                description: 'Go to cinema',
+                completed: false
+            }, {
+                description: 'Buy somme food',
+                completed: true
+            }, {
+                description: 'Study about promise today',
+                completed: false
+            },
+        ], (err, res) => {
+            if (err) {
+                return console.log('Unable to insert resources!');
+            }
+            console.log(res.ops);
+        }); */
 
-    // Create new tasks collection
-    db.collection('tasks').insertMany([
-        {
-            description: 'Go to cinema',
-            completed: false
-        }, {
-            description: 'Buy somme food',
-            completed: true
-        }, {
-            description: 'Study about promise today',
-            completed: false
-        },
-    ], (err, res) => {
-        if (err) {
-            return console.log('Unable to insert resources!');
-        }
-        console.log(res.ops);
-    });
+
+    // READ
+    // ==============================
+        // Find one document
+        // Find by id
+        // db.collection('users').findOne({_id: new ObjectID('5e557812e87dce25f480fbad')}, (err, user) => {
+        // Find by name
+        // db.collection('users').findOne({name: 'Artiom'}, (err, user) => {
+        //     if (err) {
+        //         return console.log('Unable to find the user');
+        //     }
+        //     console.log(user);
+        // });
+
+        // Find many
+        // db.collection('users').find({ age: 33, name: 'John }).toArray((err, document) => {
+        //     if (err) {
+        //         return console.log('Unable to find the user');
+        //     }
+        //     console.log(document);
+        // });
+
 });
