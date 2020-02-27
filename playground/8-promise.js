2469ae7780252e8d43efbf987b7113e238addbe2
Artiom <sinigur2006@gmail.com>
2020-02-26 19:05:49 -0500

    Promise chaining

--- playground/8-promise.js
+++ playground/8-promise.js
@@ -33,3 +33,24 @@ doWorkPromise()
         console.error(err);
     })
 
+
+// Promise Chaining
+function add(x, y) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(x + y);
+        }, 2000);
+    });
+}
+
+add(1, 3)
+    .then((sum) => {
+        console.log(sum);
+        return add(sum, 2);
+    })
+    .then((sum2) => {
+        console.log(sum2);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
