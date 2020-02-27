31b44794b0d97ab9aac93b15915b92949f1d4abf
Artiom <sinigur2006@gmail.com>
2020-02-26 20:05:09 -0500

    Playground promise chaining

---
+++ task-manager/playground/promise-chaining.js
@@ -0,0 +1,50 @@
+require('../src/db/mongoose');
+const User = require('../src/models/User');
+
+// User.findByIdAndUpdate('5e55cbd92923f45c6cbc2e7b', { age: 1 })
+//     .then((user) => {
+//         console.log(user);
+//         return User.countDocuments({ age: 1 })
+//     })
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+
+User.find({ age: 0 })
+    .then((users) => {
+        console.log(users);
+        return User.countDocuments({ age: 0 })
+    })
+    .then((res) => {
+        console.log('User with age 0: ' + res);
+        return User.updateMany({ age: 0 }, { age: 1 });
+    })
+    .then((res) => {
+        if (res.n) {
+            console.log('Success! Users with age 0 updated! ' + res.nModified);
+        } else {
+            console.log('Error! No users founded!');
+        }
+        return User.find({ age: 1 })
+    })
+    .then((users) => {
+        console.log(users);
+        return User.countDocuments({ age: 1 });
+    })
+    .then((countAll) => {
+        console.log('Users with age 1: ' + countAll);
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+
+    // find users with age 0
+    // log them
+    // count them
+    // 
+    // users with age 0 update to 1
+    // log them
+    // count them
--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
 
 // Connect and create new database task-manager-api
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true,
+    useFindAndModify: false
+});
