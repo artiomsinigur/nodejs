c6f2e3e642ffd2dc810d89bf31b7e560ef4fdfb4
Artiom <sinigur2006@gmail.com>
2020-03-05 15:18:50 -0500

    Delete avatar (clear)

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -202,4 +202,19 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
     res.status(400).send({ error: error.message });
 });
 
+/**
+ * Delete avatar
+ */
+// to display a binary file we need to add following in tag img: <img src="data:image/jpg;base64,sds3r4$..." alt="">
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    try {
+        req.user.avatar = undefined;
+        await req.user.save();
+
+        res.send();
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 module.exports = router;
